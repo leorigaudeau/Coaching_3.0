7025465339a052d7cf85364d902a9be5169df758
Unknown <evan1998@hotmail.fr>
2018-12-13 10:41:09 +0100

    matthieu boté fatal
    création page accueil

---
+++ actincoach/src/pages/accueil/accueil.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AccueilPage } from './accueil';
+
+@NgModule({
+  declarations: [
+    AccueilPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AccueilPage),
+  ],
+  exports: [
+    AccueilPage
+  ]
+})
+export class AccueilPageModule {}
