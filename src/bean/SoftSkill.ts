bf32ffbfc58eb5b6930c09b619c5b07b4dfab79c
Nair0fl <boche.florian@outlook.fr>
2019-04-25 08:03:26 +0200

    Bean d softskill

---
+++ src/bean/SoftSkill.ts
@@ -0,0 +1,13 @@
+export class SoftSkill {
+    id_softskill:string;
+    logo:string;
+    nom_softskill: string;
+    note:number
+
+    constructor(id_softskill:string,logo:string,nom_softskill:string,note:number) {
+        this.id_softskill=id_softskill;
+        this.nom_softskill=nom_softskill;
+        this.logo=logo;
+        this.note=note;
+   }
+  }
